69ca736b1a9d6a516222afa9a94cdab666a518cd
Robert Nimmo <robertnimmo26@live.co.uk>
2023-02-18 15:23:55 +0000

    removed uneeded input

--- sas/military_time_conversion/military_time_conversion_code.py
+++ sas/military_time_conversion/military_time_conversion_code.py
@@ -1,6 +1,4 @@
 from dateutil import parser
-from datetime import datetime
-
 
 time_strings = {1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five',
                 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
